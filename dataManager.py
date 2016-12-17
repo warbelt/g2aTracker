33ef274c96053921bb689fc161fe87b6cc4492c3
Mario Durántez <mdurantezs@gmail.com>
2016-12-17 12:20:23 +0100

    Convert indentations to spaces

--- dataManager.py
+++ dataManager.py
@@ -5,100 +5,100 @@ gamesDataFile = "data/gamesdata.gtd"
 
 # Manages tasks revolving saving, updating and storing games data
 class dataManager:
-	def __init__(self, scrapper):
-		self.storedData = self.loadGamesData()
-		self.scrapper = scrapper
+    def __init__(self, scrapper):
+        self.storedData = self.loadGamesData()
+        self.scrapper = scrapper
 
-	# returns stored data from previous sessions, parsed from JSON file to dictionary
-	def loadGamesData(self):
-		data = {}
+    # returns stored data from previous sessions, parsed from JSON file to dictionary
+    def loadGamesData(self):
+        data = {}
 
-		try: 
-			f = open(gamesDataFile, 'r')
-			try:
-				data = json.load(f)
-			except:
-				pass
-			f.close()
+        try: 
+            f = open(gamesDataFile, 'r')
+            try:
+                data = json.load(f)
+            except:
+                pass
+            f.close()
 
-		except IOError:
-			print("No prior data was found")
+        except IOError:
+            print("No prior data was found")
 
-		return data
+        return data
 
-	# Updates @self.storedData with data retrieved by @self.scrapper, 
-	# games with prior records are updated with the new data, and games without prior records are added
-	def update(self):
-		scrapped = self.scrapper.scrap(self.getGamesIDsList())
+    # Updates @self.storedData with data retrieved by @self.scrapper, 
+    # games with prior records are updated with the new data, and games without prior records are added
+    def update(self):
+        scrapped = self.scrapper.scrap(self.getGamesIDsList())
 
-		for game in scrapped:
-			# Only the last price retrieved each day is stored. If there is already an stored price from the present day, it is overwritten
-			date = time.strftime("%x")
+        for game in scrapped:
+            # Only the last price retrieved each day is stored. If there is already an stored price from the present day, it is overwritten
+            date = time.strftime("%x")
 
-			# If the last record is from this day, it's price is overwritten
-			if len(self.storedData[game]['records']) > 0:
-				if self.storedData[game]['records'][-1]['dateAndTime'] == date : 
-					self.storedData[game]['records'][-1]['price'] = scrapped[game]
+            # If the last record is from this day, it's price is overwritten
+            if len(self.storedData[game]['records']) > 0:
+                if self.storedData[game]['records'][-1]['dateAndTime'] == date : 
+                    self.storedData[game]['records'][-1]['price'] = scrapped[game]
 
-				else:
-					record = {'dateAndTime': date, 'price': scrapped[game]}
-					self.storedData[game]['records'].append(record)
-			# If this is the first record for the day, store if directly
-			else:
-				record = {'dateAndTime': date, 'price': scrapped[game]}
-				self.storedData[game]['records'].append(record)
+                else:
+                    record = {'dateAndTime': date, 'price': scrapped[game]}
+                    self.storedData[game]['records'].append(record)
+            # If this is the first record for the day, store if directly
+            else:
+                record = {'dateAndTime': date, 'price': scrapped[game]}
+                self.storedData[game]['records'].append(record)
 
-		self.storeGamesData()
+        self.storeGamesData()
 
-	# Writes @self.storedData formated as JSON to a file
-	def storeGamesData(self):
-		try: 
-			f = open(gamesDataFile, 'w')
-			f.write(json.dumps(self.storedData))
-			f.close()
+    # Writes @self.storedData formated as JSON to a file
+    def storeGamesData(self):
+        try: 
+            f = open(gamesDataFile, 'w')
+            f.write(json.dumps(self.storedData))
+            f.close()
 
-		except IOError:
-			print("Could not save data to 'gamesdata.gtd'")
+        except IOError:
+            print("Could not save data to 'gamesdata.gtd'")
 
-	# Returns amount of games stored in dictionary
-	def getGamesDBLen(self):
-		return len(self.storedData)
+    # Returns amount of games stored in dictionary
+    def getGamesDBLen(self):
+        return len(self.storedData)
 
-	# Returns a list of the IDs of every game in @self.storedData
-	def getGamesIDsList(self):
-		return self.storedData.keys()
+    # Returns a list of the IDs of every game in @self.storedData
+    def getGamesIDsList(self):
+        return self.storedData.keys()
 
-	# @url is the address of the product to keep track of. dataManager adds the new product with a fresh record to its dictionary
-	# Returns title of added game if successful, else returns -1
-	def addNewGame(self, url):
-		titleAndId = self.scrapper.getGameTitleAndID(url)
+    # @url is the address of the product to keep track of. dataManager adds the new product with a fresh record to its dictionary
+    # Returns title of added game if successful, else returns -1
+    def addNewGame(self, url):
+        titleAndId = self.scrapper.getGameTitleAndID(url)
 
-		# If url does not point to a valid product page, return -1
-		if titleAndId == -1 :
-			return -1
+        # If url does not point to a valid product page, return -1
+        if titleAndId == -1 :
+            return -1
 
-		price = self.scrapper.getPrice(titleAndId['id'])
+        price = self.scrapper.getPrice(titleAndId['id'])
 
-		record = {'dateAndTime' : time.strftime("%x"), 'price' : price}
-		# Check first if the game is already being tracked, if it is not, then add it to the dictionary
-		if titleAndId['id'] not in self.storedData.keys():
-			self.storedData[titleAndId['id']] = {'title' : titleAndId['title'], 'records' : [record]}
+        record = {'dateAndTime' : time.strftime("%x"), 'price' : price}
+        # Check first if the game is already being tracked, if it is not, then add it to the dictionary
+        if titleAndId['id'] not in self.storedData.keys():
+            self.storedData[titleAndId['id']] = {'title' : titleAndId['title'], 'records' : [record]}
 
-		# Save data to file after adding the product
-		self.storeGamesData()
+        # Save data to file after adding the product
+        self.storeGamesData()
 
-		return titleAndId['title']
+        return titleAndId['title']
 
-	# Removes game whose id is situated at position @index in the keys list of the prudict data dictionary
-	def removeGame(self, index):
-		self.storedData.pop(self.getGamesIDsList()[index])
-		# Save data to file after removing product
-		self.storeGamesData()
+    # Removes game whose id is situated at position @index in the keys list of the prudict data dictionary
+    def removeGame(self, index):
+        self.storedData.pop(self.getGamesIDsList()[index])
+        # Save data to file after removing product
+        self.storeGamesData()
 
-	# Returns dict of all records stored for a game with id equal to @gameID
-	def getGameRecords(self, gameID):
-		return self.storedData[gameID]['records']
+    # Returns dict of all records stored for a game with id equal to @gameID
+    def getGameRecords(self, gameID):
+        return self.storedData[gameID]['records']
 
-	# Returns title of product associated to @gameID
-	def getGameTitle(self, gameID):
-		return self.storedData[gameID]['title']
+    # Returns title of product associated to @gameID
+    def getGameTitle(self, gameID):
+        return self.storedData[gameID]['title']
--- scrapper.py
+++ scrapper.py
@@ -6,53 +6,53 @@ import json
 #Returns first appearance of a substring that is preceded by @before and followed by @after, returns -1 if not found
 #Utilized for extracting certain values from the unparsed html source
 def getSubSBetween(source, before, after):
-	try:
-		return source.split(before)[1].split(after)[0]
-	except:
-		return -1
-	return result
+    try:
+        return source.split(before)[1].split(after)[0]
+    except:
+        return -1
+    return result
 
 #Manages tasks for scrapping www.g2a.com looking for prices
 class scrapper:
-	#Retrieves the title and product ID from the url passed as @url, returns them as an id:title pair, returns -1 if url does not contain a valid pair
-	def getGameTitleAndID(self, url):
-		#Get raw html from url. Prices information is served asynchronously in a json so we use this to get the title and ID of the product, and url of said json
-		try:
-			with contextlib.closing(urllib.urlopen(url)) as handle:
-				rawHTML = handle.read()
-		except: 
-			return -1
+    #Retrieves the title and product ID from the url passed as @url, returns them as an id:title pair, returns -1 if url does not contain a valid pair
+    def getGameTitleAndID(self, url):
+        #Get raw html from url. Prices information is served asynchronously in a json so we use this to get the title and ID of the product, and url of said json
+        try:
+            with contextlib.closing(urllib.urlopen(url)) as handle:
+                rawHTML = handle.read()
+        except: 
+            return -1
 
-		# Extract title and pID from html
-		sTitle = getSubSBetween(rawHTML, "<title>", " - G2A.COM") #Game title
-		pID = getSubSBetween(rawHTML, "productID = ", ";") #internal ID used by g2a to identify each product. Neded in order to get the json url
+        # Extract title and pID from html
+        sTitle = getSubSBetween(rawHTML, "<title>", " - G2A.COM") #Game title
+        pID = getSubSBetween(rawHTML, "productID = ", ";") #internal ID used by g2a to identify each product. Neded in order to get the json url
 
-		# If no valid pair is found, return -1
-		if sTitle == -1 or pID ==  -1:
-			return -1
+        # If no valid pair is found, return -1
+        if sTitle == -1 or pID ==  -1:
+            return -1
 
-		return {'title': sTitle, 'id': pID}
+        return {'title': sTitle, 'id': pID}
 
-	#Retrieves the cheapest price available at the moment for a product given by it's @pID
-	def getPrice(self, pID):
-		#knowing the product's ID, I retrieve the json which contains all its marketplace details (seller,prices)
-		with contextlib.closing(urllib.urlopen("https://www.g2a.com/marketplace/product/auctions/?id=" + pID)) as handle:
-			json_object = json.loads(handle.read())
+    #Retrieves the cheapest price available at the moment for a product given by it's @pID
+    def getPrice(self, pID):
+        #knowing the product's ID, I retrieve the json which contains all its marketplace details (seller,prices)
+        with contextlib.closing(urllib.urlopen("https://www.g2a.com/marketplace/product/auctions/?id=" + pID)) as handle:
+            json_object = json.loads(handle.read())
 
-		#Since offers come unordered and we don't care about the seller, we extract every price and select the lowest one
-		prices = []
-		for seller in json_object['a'].values():
-			#p is the value in selected (from cookies??) currency without sign. f is the value with sign, which is problematic with currencies with special characteres such as euro
-			### TODO 
-			### aknowledge different currencies
-			prices.append(float(seller['p']))
-		return min(prices)
+        #Since offers come unordered and we don't care about the seller, we extract every price and select the lowest one
+        prices = []
+        for seller in json_object['a'].values():
+            #p is the value in selected (from cookies??) currency without sign. f is the value with sign, which is problematic with currencies with special characteres such as euro
+            ### TODO 
+            ### aknowledge different currencies
+            prices.append(float(seller['p']))
+        return min(prices)
 
-	# Gets a list if product ids, @ids. Returns a dictionary with pairs: {id:price} for each game
-	def scrap(self, ids):
-		gamesData = {}
+    # Gets a list if product ids, @ids. Returns a dictionary with pairs: {id:price} for each game
+    def scrap(self, ids):
+        gamesData = {}
 
-		for game in ids:
-			gamesData[game] = self.getPrice(game)
+        for game in ids:
+            gamesData[game] = self.getPrice(game)
 
-		return gamesData
+        return gamesData
